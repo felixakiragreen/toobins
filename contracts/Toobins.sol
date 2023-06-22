d27464a8220d578ddee6b1186dd38406e201e83c
Felix Green <work@felix.green>
2023-06-22 14:15:54 -0400

    add delegation transfer checks

--- contracts/Toobins.sol
+++ contracts/Toobins.sol
@@ -5,7 +5,7 @@ import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 import '@openzeppelin/contracts/token/ERC721/IERC721.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
 import '@openzeppelin/contracts/utils/Strings.sol';
-import "./IDelegationRegistry.sol";
+import './IDelegationRegistry.sol';
 
 contract Toobins is Ownable, ERC721 {
 	using Strings for uint256;
@@ -13,17 +13,17 @@ contract Toobins is Ownable, ERC721 {
 	constructor(
 		address _moonbirds,
 		string memory _baseTokenURI,
-      address _delegationRegistry,
-	) ERC721('Toobins', 'TOOBIN') {
+		address _delegationRegistry
+	) ERC721('Toobins', 'TOOBINS') {
 		moonbirds = _moonbirds;
 		baseTokenURI = _baseTokenURI;
-      delegationRegistry = IDelegationRegistry(_delegationRegistry);
+		delegationRegistry = IDelegationRegistry(_delegationRegistry);
 	}
 
 	address public moonbirds;
 	uint public idTracker;
 	string public baseTokenURI;
-   IDelegationRegistry public delegationRegistry;
+	IDelegationRegistry public delegationRegistry;
 
 	//
 	// ADMIN
@@ -73,38 +73,13 @@ contract Toobins is Ownable, ERC721 {
 
 	function externalMint(address to) external {
 		require(
-			_msgSender() == address(this),
+			msg.sender == address(this),
 			'Must be called from within the contract'
 		);
 
 		_safeMint(to, idTracker);
 
 		idTracker += 1;
-	}
-
-	function _requireCanTransfer(address to, uint tokenId) internal view {
-		require(_exists(0) == true, 'Toobins run has not yet started');
-
-		// TODO: add support for delegate cash
-		require(
-			_isApprovedOrOwner(_msgSender(), tokenId),
-			'ERC721: transfer caller is not owner nor approved'
-		);
-
-		require(tokenId == 0, 'Charms are soulbound and cannot be transferred');
-
-		require(
-			IERC721(moonbirds).balanceOf(to) > 0,
-			'Toobins can only be transferred to an address with a  Moonbirds'
-		);
-
-		require(balanceOf(to) == 0, 'This address already receieved Toobin');
-	}
-
-	function canTransfer(address to, uint tokenId) public view returns (bool) {
-		_requireCanTransfer(to, tokenId);
-
-		return true;
 	}
 
 	// TRANSFER
@@ -115,43 +90,50 @@ contract Toobins is Ownable, ERC721 {
 		transferOverride(msg.sender, to, 0);
 	}
 
-   function delegatedPass(address vault, address to) public {
-    if (!delegationRegistry.checkDelegateForContract(_msgSender(), vault, address(this))) revert NotDelegatedError();
+	function delegatedPass(address vault, address to) public {
+		// TODO: handle this properly
+		// if (
+		// 	!delegationRegistry.checkDelegateForContract(
+		// 		_msgSender(),
+		// 		vault,
+		// 		address(this)
+		// 	)
+		// ) revert NotDelegatedError();
 
-      //
-    transferOverride(vault, to, 0);
-   }
+		//
+		transferOverride(vault, to, 0);
+	}
 
-//    function mintForInvisibleFriends(
-//     uint256[] calldata originalIds
-//   )
-//     external
-//     payable
-//     verifySaleState(SaleState.Private)
-//     verifyTokenBasedMintEnabled
-//     verifyAmount(originalIds.length)
-//     verifyAvailableSupply(originalIds.length)
-//   {
-//     _checkOwnershipAndMarkIDsMinted(originalIds, _msgSender());
-//     _mint(_msgSender(), originalIds.length);
-//   }
+	//    function mintForInvisibleFriends(
+	//     uint256[] calldata originalIds
+	//   )
+	//     external
+	//     payable
+	//     verifySaleState(SaleState.Private)
+	//     verifyTokenBasedMintEnabled
+	//     verifyAmount(originalIds.length)
+	//     verifyAvailableSupply(originalIds.length)
+	//   {
+	//     _checkOwnershipAndMarkIDsMinted(originalIds, _msgSender());
+	//     _mint(_msgSender(), originalIds.length);
+	//   }
 
-//   function delegatedMintForInvisibleFriends(
-//     address vault,
-//     uint256[] calldata originalIds
-//   )
-//     external
-//     payable
-//     verifySaleState(SaleState.Private)
-//     verifyTokenBasedMintEnabled
-//     verifyAmount(originalIds.length)
-//     verifyAvailableSupply(originalIds.length)
-//   {
-//     if (!delegationRegistry.checkDelegateForContract(_msgSender(), vault, address(this))) revert NotDelegatedError();
+	//   function delegatedMintForInvisibleFriends(
+	//     address vault,
+	//     uint256[] calldata originalIds
+	//   )
+	//     external
+	//     payable
+	//     verifySaleState(SaleState.Private)
+	//     verifyTokenBasedMintEnabled
+	//     verifyAmount(originalIds.length)
+	//     verifyAvailableSupply(originalIds.length)
+	//   {
+	//     if (!delegationRegistry.checkDelegateForContract(_msgSender(), vault, address(this))) revert NotDelegatedError();
 
-//     _checkOwnershipAndMarkIDsMinted(originalIds, vault);
-//     _mint(_msgSender(), originalIds.length);
-//   }
+	//     _checkOwnershipAndMarkIDsMinted(originalIds, vault);
+	//     _mint(_msgSender(), originalIds.length);
+	//   }
 
 	// overriding ERC-721 transfer functions
 
@@ -209,8 +191,94 @@ contract Toobins is Ownable, ERC721 {
 		uint tokenId,
 		bytes memory _data
 	) internal {
-		_requireCanTransfer(to, tokenId);
+		_requireTransferChecks(to, tokenId);
 
 		_safeTransfer(from, to, tokenId, _data);
+	}
+
+	//
+	// transfer checks with delegation
+	//
+
+	function canTransfer(address to, uint tokenId) public view returns (bool) {
+		_requireTransferChecks(to, tokenId);
+
+		return true;
+	}
+
+	function _requireTransferChecks(address to, uint tokenId) internal view {
+		_requireBasicTransferChecks(to, tokenId);
+		_requireSpecialTransferChecks(to, tokenId);
+	}
+
+	function _requireBasicTransferChecks(
+		address to,
+		uint tokenId
+	) internal view {
+		require(_exists(0), 'Toobins run has not yet started');
+
+		require(tokenId == 0, 'Charms are soulbound and cannot be transferred');
+
+		require(balanceOf(to) == 0, 'This address already receieved Toobins');
+	}
+
+	function _requireSpecialTransferChecks(
+		address to,
+		uint tokenId
+	) internal view {
+		// owner doesn't need special checks
+		if (to == owner()) {
+			return;
+		}
+
+		// ensure the sender is the owner or approved
+		require(
+			_isApprovedOrOwner(msg.sender, tokenId),
+			'ERC721: transfer caller is not owner nor approved'
+		);
+
+		// check if they have a moonbird
+		bool toHasMoonbird = hasMoonbird(to);
+		if (toHasMoonbird) {
+			return;
+		}
+
+		// check if they have a delegated vault with a moonbird
+		bool toDelegateHasMoonbird = delegateHasMoonbird(to);
+		if (toDelegateHasMoonbird) {
+			return;
+		}
+
+		require(
+			toHasMoonbird,
+			'Toobins can only be transferred to an address with a Moonbirds'
+		);
+	}
+
+	function hasMoonbird(address owner) internal view returns (bool) {
+		return IERC721(moonbirds).balanceOf(owner) > 0;
+	}
+
+	function delegateHasMoonbird(address delegate) internal view returns (bool) {
+		return checkForMoonbirdsVault(delegate) != address(0);
+	}
+
+	function checkForMoonbirdsVault(
+		address delegate
+	) internal view returns (address) {
+		IDelegationRegistry.DelegationInfo[]
+			memory delegateInfos = delegationRegistry.getDelegationsByDelegate(
+				delegate
+			);
+
+		for (uint i = 0; i < delegateInfos.length; i++) {
+			IDelegationRegistry.DelegationInfo memory info = delegateInfos[i];
+
+			if (hasMoonbird(info.vault)) {
+				return info.vault;
+			}
+		}
+
+		return address(0);
 	}
 }
