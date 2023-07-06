7bebbacf93bab968106a152d6d3645d8194c0d75
Felix Green <work@felix.green>
2023-07-05 21:02:29 -0400

    refactor checking logic to be inside hooks

--- contracts/Toobins.sol
+++ contracts/Toobins.sol
@@ -21,7 +21,7 @@ contract Toobins is Ownable, ERC721 {
 	}
 
 	address public moonbirds;
-	uint public idTracker;
+	uint256 public idTracker = 1; // 0 is Toobins
 	string public baseTokenURI;
 	IDelegationRegistry public delegationRegistry;
 
@@ -29,11 +29,9 @@ contract Toobins is Ownable, ERC721 {
 	// ADMIN
 	//
 
-	// Please note: there is no check for a moonbird on the initiate
+	// note: no check for a moonbird on the initiate
 	function initiate(address luckyFirst) public onlyOwner {
-		require(_exists(0) == false, 'Toobins run has already started');
-
-		internalMint(luckyFirst);
+		_mint(luckyFirst, 0);
 	}
 
 	function conclude() public onlyOwner {
@@ -41,13 +39,9 @@ contract Toobins is Ownable, ERC721 {
 	}
 
 	// returns the Toobins to the (contract) owner's wallet if it gets stuck
+	// note: charm is minted automatically in _afterTokenTransfer hook
 	function yoink() public onlyOwner {
-		// get the address where the Toobins is
-		address from = ownerOf(0);
-		// return it to home base
-		_transfer(from, msg.sender, 0);
-		// leave behind a Charm
-		internalMint(from);
+		_transfer(ownerOf(0), msg.sender, 0);
 	}
 
 	function setBaseTokenURI(string memory _baseTokenURI) public onlyOwner {
@@ -64,25 +58,6 @@ contract Toobins is Ownable, ERC721 {
 		return string(abi.encodePacked(baseTokenURI, tokenId.toString()));
 	}
 
-	// MINT
-
-	// This try+catch prevents Toobins from getting trapped
-	// In the same way The Worm was trapped by The Wriggler
-	function internalMint(address to) internal {
-		try this.externalMint(to) {} catch {}
-	}
-
-	function externalMint(address to) external {
-		require(
-			msg.sender == address(this),
-			'Must be called from within the contract'
-		);
-
-		_safeMint(to, idTracker);
-
-		idTracker += 1;
-	}
-
 	// TRANSFER
 
 	// the primary transfer function
@@ -93,121 +68,52 @@ contract Toobins is Ownable, ERC721 {
 
 	// overriding ERC-721 transfer functions
 
-	function transferFrom(
+	function _beforeTokenTransfer(
 		address from,
 		address to,
-		uint tokenId
-	) public override {
-		safeTransferFrom(from, to, tokenId, '');
-	}
-
-	function safeTransferFrom(
-		address from,
-		address to,
-		uint tokenId
-	) public override {
-		safeTransferFrom(from, to, tokenId, '');
-	}
-
-	function safeTransferFrom(
-		address from,
-		address to,
-		uint tokenId,
-		bytes memory _data
-	) public override {
-		_requireBasicTransferChecks(to, tokenId);
-		_requireSpecialTransferChecks(to);
-
-		_pass(from, to, tokenId, _data);
-	}
-
-	// overriding with the following functions
-
-	function _pass(
-		address from,
-		address to,
-		uint tokenId,
-		bytes memory _data
-	) internal {
-		handleTransfer(from, to, tokenId, _data);
-
-		// if there is a moonbird, mint to sender
-		if (hasMoonbird(msg.sender)) {
-			handleMint(msg.sender);
-		} else {
-			// if delegated, mint to vault
-			address vault = checkForMoonbirdsVault(msg.sender);
-			if (vault != address(0)) {
-				handleMint(vault);
-			}
+		uint256 tokenId,
+		uint256 batchSize
+	) internal virtual override {
+		assert(batchSize == 1);
+		if (from == address(0) || to == owner()) {
+			// mints are always allowed and we don't stop the owner from yoinking
+			return;
 		}
+
+		require(tokenId == 0, 'Charms are soulbound and cannot be transferred');
+		require(balanceOf(to) == 0, 'This address already receieved Toobins');
+		require(
+			hasMoonbird(to) || checkForMoonbirdsVault(to) != address(0),
+			'Toobins can only be transferred to an address with a Moonbird'
+		);
 	}
 
-	function handleTransfer(
+	function _afterTokenTransfer(
 		address from,
-		address to,
-		uint tokenId,
-		bytes memory _data
-	) internal {
-		_requireBasicTransferChecks(to, tokenId);
-		_requireSpecialTransferChecks(to);
+		address,
+		uint256,
+		uint256 batchSize
+	) internal virtual override {
+		assert(batchSize == 1);
+		if (from == address(0)) {
+			// mints have no side effects and Charms can't be transferred so we wouldn't be here
+			return;
+		}
 
-		_safeTransfer(from, to, tokenId, _data);
-	}
-
-	function handleMint(address location) internal {
-		// do NOT mint when transferring from the (contract) owner
-		// (we don't want to leave a Charm in the Owner's address)
-		if (location != owner()) {
-			internalMint(location);
+		// @review This is to keep with the functionality as currently implemented, for demonstration purposes,
+		// but see other comments around expected behaviour.
+		address mintTo = from;
+		if (!hasMoonbird(mintTo)) {
+			mintTo = checkForMoonbirdsVault(from);
+		}
+		if (mintTo != address(0)) {
+			_mint(mintTo, idTracker++); // do NOT use safeMint as this allowed the Wriggler exploit
 		}
 	}
 
 	//
 	// transfer checks with delegation
 	//
-
-	function canTransfer(address to, uint tokenId) public view returns (bool) {
-		_requireBasicTransferChecks(to, tokenId);
-		_requireSpecialTransferChecks(to);
-
-		return true;
-	}
-
-	function _requireBasicTransferChecks(
-		address to,
-		uint tokenId
-	) internal view {
-		require(_exists(0), 'Toobins run has not yet started');
-
-		require(tokenId == 0, 'Charms are soulbound and cannot be transferred');
-
-		require(balanceOf(to) == 0, 'This address already receieved Toobins');
-
-		require(
-			_isApprovedOrOwner(msg.sender, tokenId),
-			'ERC721: caller is not token owner or approved'
-		);
-	}
-
-	function _requireSpecialTransferChecks(address to) internal view {
-		// check if they have a moonbird
-		bool toHasMoonbird = hasMoonbird(to);
-		if (toHasMoonbird) {
-			return;
-		}
-
-		// check if they have a delegated vault with a moonbird
-		bool toDelegateHasMoonbird = delegateHasMoonbird(to);
-		if (toDelegateHasMoonbird) {
-			return;
-		}
-
-		require(
-			toHasMoonbird,
-			'Toobins can only be transferred to an address with a Moonbird'
-		);
-	}
 
 	function hasMoonbird(address owner) internal view returns (bool) {
 		return IERC721(moonbirds).balanceOf(owner) > 0;
